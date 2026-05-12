a3221bd697acb5b8792029a2610cd4673eb7ba81
Jackie Chung <jackie.chung@utp.ac.pa>
2026-05-11 20:03:07 -0500

    Pantalla Principal sin Logica

--- app/src/main/java/com/example/laboratorio3hpa4/MainActivity.kt
+++ app/src/main/java/com/example/laboratorio3hpa4/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.laboratorio3hpa4
 
 import android.os.Bundle
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,9 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val listview = findViewById<ListView>(R.id.lvCategorias)
+
+
+
     }
 }
